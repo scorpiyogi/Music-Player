bfb360d2f512b51045d6074bdbd034798ebbd40c
scorpiyogi <sohani.codes@gmail.com>
2022-01-30 09:27:14 +0530

    menu

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@chakra-ui/layout"
-import Sidebar from "./sidebar"
+import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
 const PlayerLayout = ({ children }) => {
   return (
@@ -7,14 +7,14 @@ const PlayerLayout = ({ children }) => {
       <Box position="absolute" top="0" width="250px" left="0">
         <Sidebar />
       </Box>
-      <Box marginLeft="250px" marginBottom="180px">
+      <Box marginLeft="250px" marginBottom="100px">
         {children}
       </Box>
       <Box position="absolute" left="0" bottom="0">
-        player
+        PLAYER
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default PlayerLayout
+export default PlayerLayout;
